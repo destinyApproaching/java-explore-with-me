b9850ce3f027dee4b3940dbab126277126439cf6
Alex <MacMorphling@mail.ru>
2023-08-30 13:45:50 +0300

    fix: fixed validation

--- stats/service/src/main/java/ru/practicum/controller/StatsController.java
+++ stats/service/src/main/java/ru/practicum/controller/StatsController.java
@@ -2,30 +2,31 @@ package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.EndpointHitDto;
 import ru.practicum.ViewStatsDto;
 import ru.practicum.service.StatsService;
 
-import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class StatsController {
     private final StatsService statsService;
 
     @PostMapping("/hit")
-    public EndpointHitDto saveHit(@RequestBody @Valid EndpointHitDto endpointHitDto) {
+    public EndpointHitDto saveHit(@RequestBody @Validated EndpointHitDto endpointHitDto) {
         return statsService.saveHit(endpointHitDto);
     }
 
     @GetMapping("/stats")
     public List<ViewStatsDto> getStats(@RequestParam("start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
                                        @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
-                                       @RequestParam(name = "uris", required = false, defaultValue = "") List<String> uris,
-                                       @RequestParam(name = "unique", required = false, defaultValue = "false") Boolean unique) {
+                                       @RequestParam(required = false) List<String> uris,
+                                       @RequestParam(defaultValue = "false") Boolean unique) {
         return statsService.getStats(start, end, uris, unique);
     }
 }
